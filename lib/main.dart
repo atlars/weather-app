a8da02965fe572571919fd45b94254da6b6e9d0c
Lars Thomae <lars.thomae@aol.com>
2024-09-02 14:43:30 +0200

    feat: add local preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,61 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:weather_app/provider/prefs.dart';
 import 'package:weather_app/provider/theme.dart';
 import 'package:weather_app/ui/pages/weather_page.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+
+  // Preserves the native splash screen until FlutterNativeSplash.remove() is called
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
   runApp(
     const ProviderScope(
-      child: WeatherApp(),
+      child: _EagerInitialization(
+        child: WeatherApp(),
+      ),
     ),
   );
 }
 
-class WeatherApp extends ConsumerWidget {
-  const WeatherApp({super.key});
+/// Only returns the [child] when the watched providers are initialized
+class _EagerInitialization extends ConsumerWidget {
+  const _EagerInitialization({required this.child});
+  final Widget child;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final values = [
+      ref.watch(prefsProvider),
+    ];
+
+    if (values.every((value) => value.hasValue)) {
+      return child;
+    }
+
+    return const SizedBox();
+  }
+}
+
+class WeatherApp extends ConsumerStatefulWidget {
+  const WeatherApp({super.key});
+
+  @override
+  ConsumerState<WeatherApp> createState() => _WeatherAppState();
+}
+
+class _WeatherAppState extends ConsumerState<WeatherApp> {
+
+  @override
+  void initState() {
+    super.initState();
+    FlutterNativeSplash.remove();
+  }
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       title: "Weahter app",
       theme: ref.watch(lightThemeProvider),
@@ -31,9 +70,9 @@ class WeatherApp extends ConsumerWidget {
 class WebScrollBehavior extends MaterialScrollBehavior {
   // Override behavior methods and getters like dragDevices
   @override
-  Set<PointerDeviceKind> get dragDevices => { 
-    PointerDeviceKind.touch,
-    PointerDeviceKind.mouse,
-    // etc.
-  };
+  Set<PointerDeviceKind> get dragDevices => {
+        PointerDeviceKind.touch,
+        PointerDeviceKind.mouse,
+        // etc.
+      };
 }
